7811a8c49a62f35e1e511b9f157b734b14610b1e
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-11-01 18:27:37 +0000

    Update Main.java

--- src/main/java/dad/javafx/InicioSesionMVC/Main.java
+++ src/main/java/dad/javafx/InicioSesionMVC/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		/*USUARIO Y CONTRASEÑAS
-		  chuck, norris
-		  charles, pepe
-		  david, Libreria
-		*/
-		
+
+		/*
+		 * USUARIO Y CONTRASEÑAS chuck, norris 
+		 * charles, pepe 
+		 * david, Libreria
+		 */
+
 		App.main(args);
-		
+
 	}
 
 }
