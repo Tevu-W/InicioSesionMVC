97f69186613f16c7e25508e3e07e0f061661a884
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-11-01 18:27:05 +0000

    Update Main.java

--- src/main/java/dad/javafx/InicioSesionMVC/Main.java
+++ src/main/java/dad/javafx/InicioSesionMVC/Main.java
@@ -1,10 +1,20 @@
 package dad.javafx.InicioSesionMVC;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/*USUARIO Y CONTRASEÑAS
+		  chuck, norris
+		  charles, pepe
+		  david, Libreria
+		*/
+		
 		App.main(args);
+		
 	}
 
 }
