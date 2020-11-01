0461459dd6e111295df5fd1505514d9aefb0aaff
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-11-01 17:15:32 +0000

    Create App.java

---
+++ src/main/java/dad/javafx/InicioSesionMVC/App.java
@@ -0,0 +1,31 @@
+package dad.javafx.InicioSesionMVC;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+	private InicioSesionController controller;
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		controller = new InicioSesionController();
+		
+		Scene scene = new Scene(controller.getView(), 320, 200);
+		
+		Stage stage = new Stage();
+		stage.setTitle("InicioSesionMVC");
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
