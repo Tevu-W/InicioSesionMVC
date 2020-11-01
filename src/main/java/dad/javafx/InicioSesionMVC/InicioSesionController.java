b6201fa06f18f0400051110950f1c06099b158ee
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-11-01 17:15:30 +0000

    Update InicioSesionController.java

--- src/main/java/dad/javafx/InicioSesionMVC/InicioSesionController.java
+++ src/main/java/dad/javafx/InicioSesionMVC/InicioSesionController.java
@@ -1,5 +1,33 @@
 package dad.javafx.InicioSesionMVC;
 
+import javafx.event.ActionEvent;
+
 public class InicioSesionController {
 
+	private InicioSesionView view = new InicioSesionView();
+	private InicioSesionModel model = new InicioSesionModel();
+	
+	public InicioSesionController() {
+		
+		view.getAcceder().setOnAction(e -> onAccederAction(e));
+		view.getCancelar().setOnAction(e -> onCancelarAction(e));
+		
+		model.usuarioProperty().bind(view.getUsuarioField().textProperty());
+		
+	}
+	
+
+	public InicioSesionView getView() {
+		return view;
+	}
+	
+	private void onAccederAction(ActionEvent e) {
+		System.out.println(view.getUsuarioField().getText());
+		System.out.println(view.getPass().getText());
+	}
+	
+	private void onCancelarAction(ActionEvent e) {
+		
+	}
+	
 }
